9f06b847e867620997e16d84f72dd820c2fda1b8
timofey97 <gleb285@gmail.com>
2021-05-23 20:39:48 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,18 @@ import React from 'react';
 import { useRoutes } from 'hookrouter';
 import NotFoundPage from './pages/NotFound';
 import routes from './routes';
+import Header from './components/Header';
 
 const App = () => {
   const match = useRoutes(routes);
-  return match || <NotFoundPage />;
+  return match ? (
+    <>
+      <Header />
+      {match}
+    </>
+  ) : (
+    <NotFoundPage />
+  );
 };
 
 export default App;
